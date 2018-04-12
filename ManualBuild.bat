08309b712322843d16b0fad537996d575b5a5ff9
aadhithB <Aadhith.Bose@clearmatch.com>
2018-04-13 11:14:14 +1200

    Config  - Packages

--- ManualBuild.bat
+++ ManualBuild.bat
@@ -9,3 +9,6 @@ CD "%CD%\Parallel.Test.FrameworkTests\bin\Release"
 nunit3-console Parallel.Test.FrameworkTests.dll
 
 nunit3-console.exe **\Parallel.Test.FrameworkTests\bin\Release\Parallel.Test.FrameworkTests.dll
+
+REM for running the open cover
+packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -register:user -target:"packages\xunit.runner.console.2.2.0\tools\xunit.console.x86.exe" -targetargs:"YourTestDLL -noshadow" -output:".\coverage.xml" -filter:"+[YourSUTNamepace*]* -[YourTestNamespace*]*"
