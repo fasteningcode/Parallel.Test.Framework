91a4d57c74132fcbad5418cda4902a1814f67613
aadhithB <Aadhith.Bose@clearmatch.com>
2018-04-13 12:07:13 +1200

    Code Coverage

--- ManualBuild.bat
+++ ManualBuild.bat
@@ -2,6 +2,8 @@ REM ./packages/NUnit.ConsoleRunner.3.8.0/tools/nunit-console.exe ./Parallel.Test
 REM \packages\NUnit.ConsoleRunner.3.8.0\tools\nunit-console.exe Parallel.Test.FrameworkTests\bin\Release\Parallel.Test.FrameworkTests.dll
 REM for building the parallel.test.framework
 
+REM nunit3-console.exe c:\Projects\Parallel.Test.Framework\Parallel.Test.FrameworkTests\bin\Release\Parallel.Test.FrameworkTests.dll
+
 
 path %loc%;"%cd%\packages\NUnit.ConsoleRunner.3.8.0\tools"
 start nunit3-console.exe
@@ -11,4 +13,7 @@ nunit3-console Parallel.Test.FrameworkTests.dll
 nunit3-console.exe **\Parallel.Test.FrameworkTests\bin\Release\Parallel.Test.FrameworkTests.dll
 
 REM for running the open cover
+packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -register:aadhithB -target:"packages\NUnit.ConsoleRunner.3.8.0\tools\nunit3-console.exe" -targetargs:"YourTestDLL -noshadow" -output:".\coverage.xml" -filter:"+[YourSUTNamepace*]* -[YourTestNamespace*]*"
+
+
 packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -register:user -target:"packages\xunit.runner.console.2.2.0\tools\xunit.console.x86.exe" -targetargs:"YourTestDLL -noshadow" -output:".\coverage.xml" -filter:"+[YourSUTNamepace*]* -[YourTestNamespace*]*"
