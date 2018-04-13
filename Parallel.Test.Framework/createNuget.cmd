12ef51d9d879b9a5e40bd37de0c56e0b30203e8e
aadhithB <Aadhith.Bose@clearmatch.com>
2018-04-13 14:03:11 +1200

    security update

---
+++ Parallel.Test.Framework/createNuget.cmd
@@ -0,0 +1,5 @@
+nuget pack Parallel.Test.Framework.csproj -properties Configuration=Release -version 1.0.0
+
+dotnet nuget push Parallel.Test.Framework.1.0.0.nupkg  -k nugetKeyPls  -s https://www.nuget.org/
+
+pause
