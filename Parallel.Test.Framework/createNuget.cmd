4fb4edf84e7d97d3bddbfbde68f4d5ec7a5418f1
DESKTOP-9TK8BNM\aadhi <aadhi@fasteningcode.com>
2018-04-28 10:32:48 +1200

    Appveyor Changes Tag

--- Parallel.Test.Framework/createNuget.cmd
+++ Parallel.Test.Framework/createNuget.cmd
@@ -1,4 +1,4 @@
-nuget pack Parallel.Test.Framework.csproj -properties Configuration=Release -version 1.0.6
+nuget pack Parallel.Test.Framework.csproj -properties Configuration=Release -version 1.0.7
 
 REM dotnet nuget push Parallel.Test.Framework.1.0.0.nupkg  -k nugetKeyPls  -s https://www.nuget.org/
 
