8972694852fa0c9e672997135544bc88d0143eea
DESKTOP-9TK8BNM\aadhi <aadhi@fasteningcode.com>
2018-04-28 14:09:02 +1200

    Update - Dependencies

--- Parallel.Test.Framework/createNuget.cmd
+++ Parallel.Test.Framework/createNuget.cmd
@@ -1,4 +1,4 @@
-nuget pack Parallel.Test.Framework.csproj -properties Configuration=Release -version 1.0.7
+nuget pack Parallel.Test.Framework.csproj -properties Configuration=Release -version 1.0.0.67
 
 REM dotnet nuget push Parallel.Test.Framework.1.0.0.nupkg  -k nugetKeyPls  -s https://www.nuget.org/
 
