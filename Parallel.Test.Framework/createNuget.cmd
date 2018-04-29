80f572a68913055d3077fc34b04251c96978850d
Fastening Code <aadhithbose@gmail.com>
2018-04-30 08:49:32 +1200

    Update createNuget.cmd

--- Parallel.Test.Framework/createNuget.cmd
+++ Parallel.Test.Framework/createNuget.cmd
@@ -1,5 +1,5 @@
-nuget pack Parallel.Test.Framework.csproj -properties Configuration=Release -version 1.0.0.67
+nuget pack Parallel.Test.Framework.csproj -properties Configuration=Release -version 1.0.0.73
 
-REM dotnet nuget push Parallel.Test.Framework.1.0.0.nupkg  -k nugetKeyPls  -s https://www.nuget.org/
+nuget push Parallel.Test.Framework.1.0.0.73.nupkg oy2bvbq5msk4gzgoqk3q3lrmddoqoizepg6bkcfi8ov0av5ly5  -Source https://api.nuget.org/v3/index.json
 
 pause
